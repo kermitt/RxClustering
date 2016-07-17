d7dd15b9bc6e5f691dbeb9b5260313e2d402c7a8
unknown <r621362@L256753.regence.com>
2016-07-16 22:03:46 -0700

    write the 3rd pass

---
+++ common/Config.java
@@ -0,0 +1,31 @@
+package common;
+import java.util.*;
+public class Config {
+	public static String STRING = "string";
+	public static String NUMBER = "number";
+	public static String DATE = "date";
+	public static String BOOLEAN = "boolean";
+	
+	public static LinkedHashMap<String, String> getFeatures() {
+		/* return the features and the type of each feature, to be 
+		 * read in from the originating .csv file 
+		 */
+		LinkedHashMap < String, String > features = new LinkedHashMap<>();
+		features.put("person_id",STRING);
+		features.put("gender_code",STRING);
+		features.put("ccs_category_id",STRING);
+		features.put("ndc_code",STRING);
+		features.put("drug_label_name",STRING);
+		features.put("drug_group_description",STRING);
+		features.put("drug_class_description",STRING);
+		features.put("days_supply_count", NUMBER);
+		features.put("filled_date",DATE);
+		features.put("patient_paid_amount",NUMBER);
+		features.put("ingredient_cost_paid_amount",NUMBER);
+		features.put("age_filled",NUMBER);
+		features.put("after_cure",BOOLEAN);
+		features.put("during_treatment",BOOLEAN);
+
+		return features;
+	}
+}
---
+++ prep/Test_Step1.java
@@ -0,0 +1,53 @@
+package prep;
+
+import common.Caller;
+import common.Seen;
+import java.util.*;
+public class Test_Step1 {
+
+	public static  void main(String...strings) {
+		
+		Test_Step1 tests = new Test_Step1(); 
+		tests.readTestInputFile_getProperFeatures_and_getProperFeatureCounts();
+
+		
+	}
+	
+	
+	public void readTestInputFile_getProperFeatures_and_getProperFeatureCounts() {
+
+		String fullPath = "C:/sites/healthpath/data/";
+		String file = "test_22_24_29.csv";
+		String path = fullPath + file; 
+		Step1 s1 = new Step1(); 
+		s1.readFile(path); 
+		
+		// isOk = enough features? AND enough of the  features have at least 
+		// one child member with more than 1 'seen'? 
+		boolean isOk = true; 
+		for ( String feature : s1.router.keySet()) { 
+			Map < String, Seen > map = s1.router.get(feature); 
+			//Caller.note( " F=" + feature + " count " + map.size());
+			boolean anythingSeenMoreThan1Time = false;
+			for ( String value : map.keySet()) {
+				
+				if ( feature.equals("ndc_code")) {
+				//	Caller.note( " F=" + feature + " count " + map.size() + " value " + value );
+					
+				}
+				
+				
+//				Caller.note( ( map.get(value).seen > 1 ) + "\t" + value + "   " + map.get(value).seen);
+				if ( map.get(value).seen > 1 ) { 
+					anythingSeenMoreThan1Time = true;
+				} 
+			}
+Caller.log(anythingSeenMoreThan1Time + "     feature " + feature + " anythingSeenMoreThan1Time " );
+			
+			
+			
+			isOk &= anythingSeenMoreThan1Time;
+		}
+		Caller.log(isOk);
+	}
+}
